22ab80f9d1f25096d5adc236684020447316b41b
w0n9 <38560218+W0n9@users.noreply.github.com>
2021-08-28 23:53:48 +0800

    加入名字长度为20的限制

--- database/schemas.py
+++ database/schemas.py
@@ -1,26 +1,31 @@
 # 此文件用来规范数据库表里的数据
 from typing import Any, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class DoctorCreate(BaseModel):
-    doctor_name:str
-    hospital_id:int
+    doctor_name: str = Field(..., max_length=20)
+    hospital_id: int
 
     class Config:
         orm_mode = True
+
 
 class DoctorReturn(DoctorCreate):
-    doctor_id:int
+    doctor_id: int
+
 
 class HospitalCreate(BaseModel):
-    hospital_name:str
+    hospital_name: str = Field(..., max_length=20)
 
     class Config:
         orm_mode = True
 
+
 class HospitalReturn(HospitalCreate):
-    hospital_id:int
+    hospital_id: int
+
 
 # class Employee(BaseModel):
 #     doctor_id:int
@@ -29,11 +34,11 @@ class HospitalReturn(HospitalCreate):
 #     class Config:
 #         orm_mode = True
 
+
 class Journal(BaseModel):
-    timestamp:Any
-    current_doctor_id:int
-    current_hospital_id:int
+    timestamp: Any
+    current_doctor_id: int
+    current_hospital_id: int
 
     class Config:
         orm_mode = True
-
